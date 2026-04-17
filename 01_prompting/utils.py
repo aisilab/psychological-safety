43b8f9d54432d02a6cbdc720bc2532543ac57fab
Gianluca Barmina <gianlucabarmina@gmail.com>
2026-04-17 15:46:55 +0200

    Add risk categorie and clusters to prompt generations

--- 01_prompting/utils.py
+++ 01_prompting/utils.py
@@ -31,3 +31,32 @@ def split_reasoning_traces(json_path: str, reasoning_token: str = "</think>", ou
             output_json_file = json_path.replace(".json", "_splitted.json")
         with open(output_json_file, "w") as f:
             json.dump(data, f, indent=2)
+
+def add_category_to_generation():
+    gen_v0_path = "01_prompting/results/svenharms_val_v0_latest_splitted.json"
+    gen_v1_path = "01_prompting/results/svenharms_val_v1_latest_splitted.json"
+    val_data_path = "00_data/val.json"
+
+    with open(gen_v0_path, "r") as f:
+        gen_v0_data = json.load(f)
+    with open(gen_v1_path, "r") as f:
+        gen_v1_data = json.load(f)
+    with open(val_data_path, "r") as f:
+        val_data = json.load(f)
+    
+    for item_v0, item_v1, item_val in zip(gen_v0_data["results"], gen_v1_data["results"], val_data):
+        risk_cluster = item_val["risk cluster"]
+        item_v0["risk cluster"] = risk_cluster
+        item_v1["risk cluster"] = risk_cluster
+
+        risk_category = item_val["risk category"]
+        item_v0["risk category"] = risk_category
+        item_v1["risk category"] = risk_category
+    
+    with open(gen_v0_path.replace(".json", "_with_category.json"), "w") as f:
+        json.dump(gen_v0_data, f, indent=2)
+    with open(gen_v1_path.replace(".json", "_with_category.json"), "w") as f:
+        json.dump(gen_v1_data, f, indent=2)
+
+if __name__ == "__main__":
+    add_category_to_generation()
